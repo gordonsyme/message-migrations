88c5b4abdd63a46fe5aa5ba4b9557c75d08658e7
Gordon Syme <gordonsyme@gmail.com>
2021-07-02 12:51:18 +0100

    Remove the old field name
    All clients have to be sending updated messages now so we can get rid of
    the old field name:
    ```
    $ curl --request POST --data '{"counter-value": 12, "increment-by": 3}' http://localhost:3031/message
    {"counter":15}
    ```
    
    It's an error to pass the old field name, even if you pass the new one:
    ```$ curl --request POST --data '{"counter-value": 12, "counter": 2, "increment-by": 3}' http://localhost:3031/message
    Input to process-message does not match schema:
    
    	   [(named {:counter disallowed-key} message)]
    ```

--- src/message_migrations/core.clj
+++ src/message_migrations/core.clj
@@ -4,16 +4,12 @@
 ;; What to demo?
 ;; 1 - adding a required field ✅
 ;; 2 - removing a field ✅
-;; 3 - renaming a field
+;; 3 - renaming a field ✅
 ;; 4 - changing the type of a field
 
 (def Message
   "A schema for messages that we're going to evolve over time"
-  ;; Renaming a field combines the techniques to add a field and remove a field
-  ;; Now we require the new field name. We could disallow the old name at the
-  ;; same time, but we'll accept it a bit longer
-  {(s/optional-key :counter) s/Num
-   :counter-value s/Num
+  {:counter-value s/Num
    :increment-by s/Num})
 
 (s/defn ^:always-validate process-message
