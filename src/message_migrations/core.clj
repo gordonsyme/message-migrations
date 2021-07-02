3f1bf3736f6b6ddb9d26e206ae403f028fd269d3
Gordon Syme <gordonsyme@gmail.com>
2021-07-02 12:28:17 +0100

    Remove the name field
    Once clients are updated and aren't sending this field any more we can
    remove it from the code:
    ```
    $ curl --request POST --data '{"counter": 2, "increment-by": 3}' http://localhost:3031/message
    {"counter":5}
    ```
    
    It's now an error to send the "name" field:
    ```
    $ curl --request POST --data '{"name": "Important Counter", "counter": 2, "increment-by": 3}' http://localhost:3031/message
    Input to process-message does not match schema:
    
    	   [(named {:name disallowed-key} message)]
    ```

--- src/message_migrations/core.clj
+++ src/message_migrations/core.clj
@@ -3,14 +3,13 @@
 
 ;; What to demo?
 ;; 1 - adding a required field ✅
-;; 2 - removing a field
+;; 2 - removing a field ✅
 ;; 3 - renaming a field
 ;; 4 - changing the type of a field
 
 (def Message
   "A schema for messages that we're going to evolve over time"
-  {(s/optional-key :name) s/Str
-   :counter s/Num
+  {:counter s/Num
    :increment-by s/Num})
 
 (s/defn ^:always-validate process-message
