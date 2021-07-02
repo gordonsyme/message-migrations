38af07c6e678da539be590de1cc66ad2080203e2
Gordon Syme <gordonsyme@gmail.com>
2021-07-02 12:49:20 +0100

    Require the new name for the field
    ```
    $ curl --request POST --data '{"counter-value": 12, "increment-by": 3}' http://localhost:3031/message
    {"counter":15}
    ```
    
    We'll still accept the old one, but we ignore it:
    ```
    $ curl --request POST --data '{"counter-value": 12, "counter": 2, "increment-by": 3}' http://localhost:3031/message
    {"counter":15}
    ```
    
    But now it's an error not to pass the new one:
    ```
    $ curl --request POST --data '{"counter": 2, "increment-by": 3}' http://localhost:3031/message
    Input to process-message does not match schema:
    
    	   [(named {:counter-value missing-required-key} message)]
    ```

--- src/message_migrations/core.clj
+++ src/message_migrations/core.clj
@@ -10,24 +10,14 @@
 (def Message
   "A schema for messages that we're going to evolve over time"
   ;; Renaming a field combines the techniques to add a field and remove a field
-  ;; First let's make the old field name optional, and add a new optional field
-  ;; for the new name.
-  ;;
-  ;; We also need to decide what the application behaviour should be. In this
-  ;; case we will:
-  ;; * Require at least one of the keys
-  ;; * If both keys are passed, accept the new one
-  ;; * The output message format will still use `:counter`
+  ;; Now we require the new field name. We could disallow the old name at the
+  ;; same time, but we'll accept it a bit longer
   {(s/optional-key :counter) s/Num
-   (s/optional-key :counter-value) s/Num
+   :counter-value s/Num
    :increment-by s/Num})
 
 (s/defn ^:always-validate process-message
   [message :- Message]
-  (let [{:keys [increment-by counter counter-value]} message]
-    (when (and (nil? counter)
-               (nil? counter-value))
-      (throw (ex-info "Must pass one of \"counter\" or \"counter-value\"" {})))
-    (-> {:counter (or counter-value
-                      counter)}
+  (let [{:keys [increment-by counter-value]} message]
+    (-> {:counter counter-value}
         (update :counter + increment-by))))
