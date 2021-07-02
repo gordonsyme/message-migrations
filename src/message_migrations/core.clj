6a306bd144ccb55096336c18c3ce83054e4de2a7
Gordon Syme <gordonsyme@gmail.com>
2021-07-02 12:43:12 +0100

    Prepare to rename the `counter` field to `counter-value`
    First we accept either of the field names. We make sure that we define
    behaviour when both fields are passed. We could choose to make it an
    error, or accept the new field over the old field. In this case, we take
    the new field over the old one.
    Clients should either be old clients sending the old value, or updated
    clients sending the new value.
    
    Using the old name is OK:
    ```
    $ curl --request POST --data '{"counter": 2, "increment-by": 3}' http://localhost:3031/message
    {"counter":5}
    ```
    
    So is using the new name:
    ```
    $ curl --request POST --data '{"counter-value": 2, "increment-by": 3}' http://localhost:3031/message
    {"counter":5}
    ```
    
    Passing both is OK, but we won't encounter clients that do this:
    ```
    $ curl --request POST --data '{"counter-value": 12, "counter": 2, "increment-by": 3}' http://localhost:3031/message
    {"counter":15}
    ```
    
    But we have to pass at least one:
    ```
    $ curl --request POST --data '{"increment-by": 3}' http://localhost:3031/message
    Must pass one of "counter" or "counter-value"
    ```

--- src/message_migrations/core.clj
+++ src/message_migrations/core.clj
@@ -9,11 +9,25 @@
 
 (def Message
   "A schema for messages that we're going to evolve over time"
-  {:counter s/Num
+  ;; Renaming a field combines the techniques to add a field and remove a field
+  ;; First let's make the old field name optional, and add a new optional field
+  ;; for the new name.
+  ;;
+  ;; We also need to decide what the application behaviour should be. In this
+  ;; case we will:
+  ;; * Require at least one of the keys
+  ;; * If both keys are passed, accept the new one
+  ;; * The output message format will still use `:counter`
+  {(s/optional-key :counter) s/Num
+   (s/optional-key :counter-value) s/Num
    :increment-by s/Num})
 
 (s/defn ^:always-validate process-message
   [message :- Message]
-  (let [increment-by (:increment-by message)]
-    {:counter (+ (:counter message)
-                 increment-by)}))
+  (let [{:keys [increment-by counter counter-value]} message]
+    (when (and (nil? counter)
+               (nil? counter-value))
+      (throw (ex-info "Must pass one of \"counter\" or \"counter-value\"" {})))
+    (-> {:counter (or counter-value
+                      counter)}
+        (update :counter + increment-by))))
